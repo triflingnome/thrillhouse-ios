b993d8c6b53cf8d7a170d64048690b56c730a36b
Michael Berg <michael.rooney.berg@gmail.com>
2018-05-15 20:37:13 -0500

    Adding audio player page
    - allowing HTTP requests

---
+++ thrillhouse/thrillhouse/ThrillhousePlayerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThrillhousePlayerViewController.swift
+//  thrillhouse
+//
+//  Created by Michael Berg on 5/15/18.
+//  Copyright © 2018 Michael Berg. All rights reserved.
+//
+
+import AVKit
+import UIKit
+
+class ThrillhousePlayerViewController: AVPlayerViewController {
+
+    // MARK: View Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
